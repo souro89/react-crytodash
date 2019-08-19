fc2453f0b372fbecd44951f2af459212bafc18c7
souro89 <souro89@gmail.com>
2019-08-19 15:48:35 +0400

    Initial Commit

--- App/index.js
+++ App/index.js
@@ -1,14 +1,14 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component{
+class App extends Component {
 
-  render(){
-   return(
-     <div>
-     <p>Hello World !</p>
-     </div>
-   ); 
+  render() {
+    return (
+      <div>
+        <p>Hello World !</p>
+      </div>
+    );
   }
 
 }
