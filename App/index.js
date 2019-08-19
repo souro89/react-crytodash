b19e794d98636157df606721a025c86a2ce6da11
souro89 <souro89@gmail.com>
2019-08-19 16:21:49 +0400

    Added AppBar

--- App/index.js
+++ App/index.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import styled,{css} from 'styled-components';
+import AppLayout from './AppLayout.js'
 
 
 class App extends Component {
 
   render() {
     return (
-      <div>
+      <AppLayout>
         <WelcomeMessage />
 
-      </div>
+      </AppLayout>
     );
   }
 
