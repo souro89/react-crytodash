7f5ca6766fb79ccbdee0ad17017ea2dafe0e5e51
souro89 <souro89@gmail.com>
2019-08-19 16:14:32 +0400

    Added GoogleFonts

--- App/index.js
+++ App/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import styled,{css} from 'styled-components';
+
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <p>Hello World !</p>
+        <WelcomeMessage />
+
       </div>
     );
   }
